1395cb7bad15ea4a2aa1ea6c04de7c63ee841852
Peter Dawyndt <peter.dawyndt@ugent.be>
2020-04-10 18:08:43 +0200

    update tests of exercise 9.4

--- chapters/09 recursion/04 exercises/04 lowercase letters/solution/solution.en.py
+++ chapters/09 recursion/04 exercises/04 lowercase letters/solution/solution.en.py
@@ -1,9 +1,8 @@
 def get_input( prompt ):
     value = input( prompt )
-    for letter in value:
-        if letter < 'a' or letter > 'z':
-            print( "The character", letter, "is not allowed!")
-            value = get_input( prompt ) # DO NOT DO THIS!
+    if not value.islower():
+        print( "Invalid answer!")
+        value = get_input( prompt ) # DO NOT DO THIS!
     return value
 
 s = get_input( "Give a string of lower case letters: " )
