9bb89149edfb9af6ae7a338817b1f8c6a4fecb29
Peter Dawyndt <peter.dawyndt@ugent.be>
2020-04-15 07:24:42 +0200

    update tests for exercise 14.2

--- chapters/14 sets/05 exercises/01 socrates/solution/solution.nl.py
+++ chapters/14 sets/05 exercises/01 socrates/solution/solution.nl.py
@@ -1,4 +1,4 @@
-alles = {'Socrates', 'Plato', 'Eratosthenes', 'Zeus', 'Hera',  'Athene', 'Acropolis', 'Kat', 'Hond'}
+alles = {'Socrates', 'Plato', 'Eratosthenes', 'Zeus', 'Hera', 'Athene', 'Acropolis', 'Kat', 'Hond'}
 mensen = {'Socrates', 'Plato', 'Eratosthenes'}
 sterfelijken = {'Socrates', 'Plato', 'Eratosthenes', 'Kat', 'Hond'}
 
